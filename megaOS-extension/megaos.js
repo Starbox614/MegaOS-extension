002b868f210c036db5ab218648f97d738b4f991a
Starbox614 <148731079+Starbox614@users.noreply.github.com>
2023-10-24 11:37:55 -0600

    Update megaos.js
    added iframe extension

--- megaOS-extension/megaos.js
+++ megaOS-extension/megaos.js
@@ -53,6 +53,77 @@ SnapExtensions.primitives.set(
 );
 
 
+SnapExtensions.primitives.set(
+  'iframe_makeSnapIframe(url,x,y)',
+  function (url, x, y) {
+   let iframe = document.createElement("iframe")
+iframe.setAttribute("width",480)
+iframe.setAttribute("height",390)
+iframe.setAttribute("frameBorder",0)
+iframe.setAttribute("allowfullscreen",true)
+iframe.setAttribute("allow", "geolocation; microphone;camera")
+iframe.setAttribute("style","position:absolute; top:"+y+"px; left:"+x+"px; z-index:50;")
+iframe.setAttribute("src",url)
+document.getElementById("world").after(iframe)
+return iframe
+  }
+);
+
+SnapExtensions.primitives.set(
+  'iframe_PutIframeOnMe(frame, offw, offh, offx, offy)',
+  function (iframe, w, h, x, y) {
+   let stage = world.children[0].stage;
+iframe.style.top = (this.position().y + y).toString() + "px";
+iframe.style.left = (this.position().x + x).toString() + "px";
+iframe.style.width = (this.width() + w).toString() + "px" ;
+iframe.style.height = (this.height() + h).toString() + "px";
+  }
+);
+
+SnapExtensions.primitives.set(
+  'iframe_deleteAllIframes',
+  function () {
+   let iframes = document.getElementsByTagName("iframe")
+
+for (i = 0;i<=iframes.length;i++) {
+iframes[i].remove();
+}
+  }
+);
+
+SnapExtensions.primitives.set(
+  'iframe_deleteHTMLElement(element)',
+  function (iframe) {
+   iframe.remove()
+  }
+);
+
+SnapExtensions.primitives.set(
+  'iframe_hide(iframe)',
+  function (iframe) {
+   iframe.style.zIndex = -1
+  }
+);
+
+SnapExtensions.primitives.set(
+  'iframe_show(iframe)',
+  function (iframe) {
+   iframe.style.zIndex = 1
+  }
+);
+
+SnapExtensions.primitives.set(
+  'iframe_hideAll',
+  function () {
+   let iframes = document.getElementsByTagName("iframe")
+
+for (i = 0;i<=iframes.length;i++) {
+iframes[i].style.zIndex = -1
+}
+  }
+);
+
+
 // I got this from @ego-lay_atman-bay
 
 /* (original comment)
