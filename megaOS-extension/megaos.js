5126a94343c1359cb4b42bfbfcdaca58b8a714df
Starbox614 <148731079+Starbox614@users.noreply.github.com>
2023-10-24 11:28:06 -0600

    Update megaos.js
    added script pic extension to it

--- megaOS-extension/megaos.js
+++ megaOS-extension/megaos.js
@@ -51,3 +51,23 @@ SnapExtensions.primitives.set(
    document.title = tab_name
   }
 );
+
+
+// I got this from @ego-lay_atman-bay
+
+/* (original comment)
+script based on @alessandrito123's script https://forum.snap.berkeley.edu/t/how-do-you-get-the-script-pic-of-a-script-using-js/9765/2
+*/
+
+SnapExtensions.primitives.set(
+    'script_pic(script)',
+    function(script) {
+        script = script.expression;
+        if (!script)
+            return new Costume(newCanvas(new Point(0, 0), true), "empty");
+        if (script instanceof Array) {
+            script = script[0];
+        }
+        return new Costume(script.fullImage(), script.selector);
+    }
+);
